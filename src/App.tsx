79efad3bc51b2a9cdc9cafbf28e9a58a467d3f54
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-14 16:04:46 -0300

    feat: integrate MenuProvider and AppRoutes in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-function App() {
+import { BrowserRouter as Router } from "react-router-dom"
+import { Header } from "./components/Header"
+import { MenuProvider } from "./contexts/MenuProvider"
+import { AppRoutes } from "./routes/AppRoutes"
 
+function App() {
   return (
-    <>
-    </>
+    <Router>
+      <MenuProvider>
+        <Header/>
+        <AppRoutes/>
+      </MenuProvider>
+    </Router>
   )
 }
 
