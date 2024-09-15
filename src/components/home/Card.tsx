e5e21a8dd6ef362972ddaf2d777039beb0e54060
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-15 13:11:45 -0300

    style: Card size adjustment and margin/padding in Objectives

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 export function Card({ text, url }: CardProps) {
     return (
         <div className="shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] 
-        max-w-64 min-w-64 2xl:max-w-80 2xl:min-w-80 max-h-40 min-h-40 sm:max-h-48 sm:min-h-48 text-center p-5 border-b-4 border-emerald-700 mb-6 ">
+        max-w-64 min-w-64 2xl:max-w-max max-h-40 min-h-40 sm:max-h-48 sm:min-h-48 text-center p-5 border-b-4 border-emerald-700 mb-6 ">
             <img src={url} alt="icone" className="w-9 mx-auto" />
             <p className="text-base sm:text-lg lg:text-xl tracking-wide">{text}</p>
         </div>
--- src/components/home/Objectives.tsx
+++ src/components/home/Objectives.tsx
@@ -2,14 +2,14 @@ import { Card } from "./Card";
 
 export function Objectives() {
     return (
-        <div className="text-center mt-5 pb-32 sm:pb-44 lg:pb-24 2xl:pb-28">
+        <div className="text-center mt-5 pb-32 sm:pb-44 lg:pb-24 2xl:pb-28 pt-5">
             <div>
-                <h1 className='text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl font-semibold text-emerald-700 mb-4'>
+                <h1 className='text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl font-semibold text-emerald-700 mb-10'>
                     Nossos Objetivos
                 </h1>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-10 pt-5 justify-items-center ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-10 pt-5 justify-items-center 2xl:mx-24">
                 <Card url="https://img.icons8.com/?size=100&id=59800&format=png&color=000000" text="Facilitar a inclusão digital da população idosa." />
                 <Card url="https://img.icons8.com/?size=100&id=61093&format=png&color=000000" text="Ajudar a aumentar a confiança ao navegar na internet." />
                 <Card url="https://img.icons8.com/?size=100&id=cseATCASNDzw&format=png&color=000000" text="Oferecer dicas e ferramentas para o uso de tecnologia." />
