dac916a9b0d1054131f4cd1f1b233fe33cc376e2
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-15 16:38:11 -0300

    feat: add route Support

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Tools } from '../pages/Tools';
 import { Tutorials } from '../pages/Tutorials';
+import { Support } from '../pages/Support';
 
 export function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ export function AppRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/tools" element={<Tools />} />
             <Route path='/tutorials' element={<Tutorials />} />
+            <Route path='/support' element={<Support />} />
         </Routes>
     )
 }
