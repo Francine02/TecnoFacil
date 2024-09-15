83823924f9d540459d8528f384de0081fd988895
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-15 15:48:18 -0300

    feat: route tutorials

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Tools } from '../pages/Tools';
+import { Tutorials } from '../pages/Tutorials';
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/tools" element={<Tools />} />
-
+            <Route path='/tutorials' element={<Tutorials />} />
         </Routes>
     )
 }
